5bf95c0ecc99c0d10109ca0f5ee5fc05f9d26c26
pkdcryptos <33589724+pkdcryptos@users.noreply.github.com>
2020-05-24 03:02:03 -0700

    Update chineseToEnglish.js

--- chineseToEnglish.js
+++ chineseToEnglish.js
@@ -1,208 +1,52 @@
-/**
- * Ant Design Pro v4 use `@ant-design/pro-layout` to handle Layout.
- * You can view component api by:
- * https://github.com/ant-design/ant-design-pro-layout
- */
-import ProLayout, { DefaultFooter, SettingDrawer } from '@ant-design/pro-layout';
-import React, { useEffect } from 'react';
-import { Link, useIntl, connect } from 'umi';
-import { GithubOutlined } from '@ant-design/icons';
-import { Result, Button } from 'antd';
-import Authorized from '@/utils/Authorized';
-import RightContent from '@/components/GlobalHeader/RightContent';
-import { getAuthorityFromRouter } from '@/utils/utils';
-import logo from '../assets/logo.svg';
-import {  RouteContext } from '@ant-design/pro-layout';
-import FooterToolbar from './components/FooterToolbar';
-import Pusher from "pusher-js";
-import { PusherProvider } from "./PusherContext";
+import React from 'react';
+import { PageLoading } from '@ant-design/pro-layout';
+import { Redirect, connect } from 'umi';
+import { stringify } from 'querystring';
 
-// Enable pusher logging - don't include this in production
-Pusher.logToConsole = true;
+class SecurityLayout extends React.Component {
+  state = {
+    isReady: false,
+  };
 
-// Set up pusher instance with main channel subscription
-// Be able to subscribe to the same channel in another component
-// with separate callback but utilizing the existing connection
-const pusher = new Pusher('413caa7397eebba49079', {
-  cluster: 'mt1',
-  encrypted: true
-});
-
-
-const noMatch = (
-  <Result
-    status={403}
-    title="403"
-    subTitle="Sorry, you are not authorized to access this page."
-    extra={
-      <Button type="primary">
-        <Link to="/user/login">Go Login</Link>
-      </Button>
-    }
-  />
-);
-
-/**
- * use Authorized check all menu item
- */
-const menuDataRender = menuList =>
-  menuList.map(item => {
-    const localItem = { ...item, children: item.children ? menuDataRender(item.children) : [] };
-    return Authorized.check(item.authority, localItem, null);
-  });
-
-const defaultFooterDom = (
-  <DefaultFooter
-    copyright="2019 蚂蚁金服体验技术部出品"
-    links={[
-      {
-        key: 'Ant Design Pro',
-        title: 'Ant Design Pro',
-        href: 'https://pro.ant.design',
-        blankTarget: true,
-      },
-      {
-        key: 'github',
-        title: <GithubOutlined />,
-        href: 'https://github.com/ant-design/ant-design-pro',
-        blankTarget: true,
-      },
-      {
-        key: 'Ant Design',
-        title: 'Ant Design',
-        href: 'https://ant.design',
-        blankTarget: true,
-      },
-    ]}
-  />
-);
-
-const BasicLayout = props => {
-  const {
-    dispatch,
-    children,
-    settings,
-    location = {
-      pathname: '/',
-    },
-  } = props;
-  /**
-   * constructor
-   */
-
-  useEffect(() => {
-    if (dispatch) {
-      dispatch({
-        type: 'user/fetchCurrent',
-      });
-    }
-  }, []);
-  /**
-   * init variables
-   */
-
-const handleLinkClick = () => {
+  componentDidMount() {
+    this.setState({
+      isReady: true,
+    });
+    
 	
-	
-       
-          if (dispatch) {
-      dispatch({
-        type: 'global/changeLayoutCollapsed',
-        payload: true
-      });
-    }
-       
-     
 	
   }
-  const handleMenuCollapse = payload => {
-    if (dispatch) {
-      dispatch({
-        type: 'global/changeLayoutCollapsed',
-        payload,
-      });
+  
+  componentWillUpdate() {
+	  
+  }
+  
+  
+
+  render() {
+    const { isReady } = this.state;
+    const { children, loading, currentUser } = this.props; // You can replace it to your authentication rule (such as check token exists)
+    // 你可以把它替换成你自己的登录认证规则（比如判断 token 是否存在）
+
+    //const isLogin = currentUser && currentUser.userid;
+	const isLogin = localStorage.getItem('antd-pro-authority')  // if token exists, it is logged in from client perspective
+    const queryString = stringify({
+      redirect: window.location.href,
+    });
+
+    if ((!isLogin && loading) || !isReady) {
+      return <PageLoading />;
     }
-  }; // get children authority
 
-  const authorized = getAuthorityFromRouter(props.route.routes, location.pathname || '/') || {
-    authority: undefined,
-  };
-  const { formatMessage } = useIntl();
-  return (
-    <>
-	<PusherProvider pusher={pusher}>
-      <ProLayout
-        logo={logo}
-        formatMessage={formatMessage}
-        menuHeaderRender={(logoDom, titleDom) => (
-          <Link to="/">
-            {logoDom}
-            {titleDom}
-          </Link>
-        )}
-        onCollapse={handleMenuCollapse}
-        menuItemRender={(menuItemProps, defaultDom) => {
-          if (menuItemProps.isUrl || menuItemProps.children || !menuItemProps.path) {
-            return defaultDom;
-          }
+    if (!isLogin && window.location.pathname !== '/user/login') {
+      return <Redirect to={`/user/login?${queryString}`} />;
+    }
 
+    return children;
+  }
+}
 
-          return  (<RouteContext.Consumer>
-                {({ isMobile }) => (
-                  isMobile ? <Link to={menuItemProps.path} onClick={handleLinkClick}>{defaultDom}</Link>  : <Link to={menuItemProps.path} >{defaultDom}</Link> 
-                )}
-              </RouteContext.Consumer>)
-			  
-        }}
-        breadcrumbRender={(routers = []) => [
-          {
-            path: '/',
-            breadcrumbName: formatMessage({
-              id: 'menu.home',
-            }),
-          },
-          ...routers,
-        ]}
-        itemRender={(route, params, routes, paths) => {
-          const first = routes.indexOf(route) === 0;
-          return first ? (
-            <Link to={paths.join('/')}>{route.breadcrumbName}</Link>
-          ) : (
-            <span>{route.breadcrumbName}</span>
-          );
-        }}
-        footerRender={() => defaultFooterDom}
-        menuDataRender={menuDataRender}
-        rightContentRender={() => <RightContent />}
-        {...props}
-        {...settings}
-      >
-        <Authorized authority={authorized.authority} noMatch={noMatch}>
-          {children}
-		  
-		   <FooterToolbar>
-       
-        <Button type="primary" >
-          提交
-        </Button>
-      </FooterToolbar>
-        </Authorized>
-      </ProLayout>
-	  </PusherProvider>
-      <SettingDrawer
-        settings={settings}
-        onSettingChange={config =>
-          dispatch({
-            type: 'settings/changeSetting',
-            payload: config,
-          })
-        }
-      />
-    </>
-  );
-};
-
-export default connect(({ global, settings }) => ({
-  collapsed: global.collapsed,
-  settings,
-}))(BasicLayout);
+export default connect(({ user, loading }) => ({
+  currentUser: user.currentUser,
+  loading: loading.models.user,
+}))(SecurityLayout);
