5632a2c495372db0f5a63b449bca9956e43c1ce5
pkdcryptos <33589724+pkdcryptos@users.noreply.github.com>
2020-05-24 02:55:08 -0700

    Update chineseToEnglish.js

--- chineseToEnglish.js
+++ chineseToEnglish.js
@@ -1,49 +1,252 @@
-import { GlobalOutlined } from '@ant-design/icons';
-import { Menu } from 'antd';
-import { getLocale, setLocale } from 'umi';
-import React from 'react';
-import classNames from 'classnames';
-import HeaderDropdown from '../HeaderDropdown';
-import styles from './index.less';
+import { PlusOutlined } from '@ant-design/icons';
+import { Button, Divider, Input, Popconfirm, Table, message } from 'antd';
+import React, { useState } from 'react';
+import styles from '../style.less';
 
-const SelectLang = props => {
-  const { className } = props;
-  const selectedLang = getLocale();
+const TableForm = ({ value, onChange }) => {
+  const [clickedCancel, setClickedCancel] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [cacheOriginData, setCacheOriginData] = useState({});
+  const [data, setData] = useState(value);
 
-  const changeLang = ({ key }) => setLocale(key);
+  const getRowByKey = (key, newData) => (newData || data)?.filter(item => item.key === key)[0];
 
-  const locales = ['zh-CN', 'zh-TW', 'en-US', 'pt-BR'];
-  const languageLabels = {
-    'zh-CN': '简体中文',
-    'zh-TW': '繁体中文',
-    'en-US': 'English',
-    'pt-BR': 'Português',
+  const toggleEditable = (e, key) => {
+    e.preventDefault();
+    const newData = data?.map(item => ({ ...item }));
+    const target = getRowByKey(key, newData);
+
+    if (target) {
+      // 进入编辑状态时保存原始数据
+      if (!target.editable) {
+        cacheOriginData[key] = { ...target };
+        setCacheOriginData(cacheOriginData);
+      }
+
+      target.editable = !target.editable;
+      setData(newData);
+    }
   };
-  const languageIcons = {
-    'zh-CN': '🇨🇳',
-    'zh-TW': '🇭🇰',
-    'en-US': '🇺🇸',
-    'pt-BR': '🇧🇷',
+
+  const newMember = () => {
+    const newData = data?.map(item => ({ ...item })) || []; // eslint-disable-next-line no-unused-expressions
+
+    newData?.push({
+      key: `NEW_TEMP_ID_${index}`,
+      workId: '',
+      name: '',
+      department: '',
+      editable: true,
+      isNew: true,
+    });
+    setIndex(index + 1);
+    setData(newData);
   };
-  const langMenu = (
-    <Menu className={styles.menu} selectedKeys={[selectedLang]} onClick={changeLang}>
-      {locales.map(locale => (
-        <Menu.Item key={locale}>
-          <span role="img" aria-label={languageLabels[locale]}>
-            {languageIcons[locale]}
-          </span>{' '}
-          {languageLabels[locale]}
-        </Menu.Item>
-      ))}
-    </Menu>
-  );
+
+  const remove = key => {
+    const newData = data?.filter(item => item.key !== key);
+    setData(newData);
+
+    if (onChange) {
+      onChange(newData);
+    }
+  };
+
+  const handleFieldChange = (e, fieldName, key) => {
+    const newData = [...data];
+    const target = getRowByKey(key, newData);
+
+    if (target) {
+      target[fieldName] = e.target.value;
+      setData(newData);
+    }
+  };
+
+  const saveRow = (e, key) => {
+    e.persist();
+    setLoading(true);
+    setTimeout(() => {
+      if (clickedCancel) {
+        setClickedCancel(false);
+        return;
+      }
+
+      const target = getRowByKey(key) || {};
+
+      if (!target.workId || !target.name || !target.department) {
+        message.error('请填写完整成员信息。');
+        e.target.focus();
+        setLoading(false);
+        return;
+      }
+
+      delete target.isNew;
+      toggleEditable(e, key);
+
+      if (onChange) {
+        onChange(data);
+      }
+
+      setLoading(false);
+    }, 500);
+  };
+
+  const handleKeyPress = (e, key) => {
+    if (e.key === 'Enter') {
+      saveRow(e, key);
+    }
+  };
+
+  const cancel = (e, key) => {
+    setClickedCancel(true);
+    e.preventDefault();
+    const newData = [...data]; // 编辑前的原始数据
+
+    let cacheData = [];
+    cacheData = newData.map(item => {
+      if (item.key === key) {
+        if (cacheOriginData[key]) {
+          const originItem = { ...item, ...cacheOriginData[key], editable: false };
+          delete cacheOriginData[key];
+          setCacheOriginData(cacheOriginData);
+          return originItem;
+        }
+      }
+
+      return item;
+    });
+    setData(cacheData);
+    setClickedCancel(false);
+  };
+
+  const columns = [
+    {
+      title: '成员姓名',
+      dataIndex: 'name',
+      key: 'name',
+      width: '20%',
+      render: (text, record) => {
+        if (record.editable) {
+          return (
+            <Input
+              value={text}
+              autoFocus
+              onChange={e => handleFieldChange(e, 'name', record.key)}
+              onKeyPress={e => handleKeyPress(e, record.key)}
+              placeholder="成员姓名"
+            />
+          );
+        }
+
+        return text;
+      },
+    },
+    {
+      title: '工号',
+      dataIndex: 'workId',
+      key: 'workId',
+      width: '20%',
+      render: (text, record) => {
+        if (record.editable) {
+          return (
+            <Input
+              value={text}
+              onChange={e => handleFieldChange(e, 'workId', record.key)}
+              onKeyPress={e => handleKeyPress(e, record.key)}
+              placeholder="工号"
+            />
+          );
+        }
+
+        return text;
+      },
+    },
+    {
+      title: '所属部门',
+      dataIndex: 'department',
+      key: 'department',
+      width: '40%',
+      render: (text, record) => {
+        if (record.editable) {
+          return (
+            <Input
+              value={text}
+              onChange={e => handleFieldChange(e, 'department', record.key)}
+              onKeyPress={e => handleKeyPress(e, record.key)}
+              placeholder="所属部门"
+            />
+          );
+        }
+
+        return text;
+      },
+    },
+    {
+      title: '操作',
+      key: 'action',
+      render: (text, record) => {
+        if (!!record.editable && loading) {
+          return null;
+        }
+
+        if (record.editable) {
+          if (record.isNew) {
+            return (
+              <span>
+                <a onClick={e => saveRow(e, record.key)}>添加</a>
+                <Divider type="vertical" />
+                <Popconfirm title="是否要删除此行？" onConfirm={() => remove(record.key)}>
+                  <a>删除</a>
+                </Popconfirm>
+              </span>
+            );
+          }
+
+          return (
+            <span>
+              <a onClick={e => saveRow(e, record.key)}>保存</a>
+              <Divider type="vertical" />
+              <a onClick={e => cancel(e, record.key)}>取消</a>
+            </span>
+          );
+        }
+
+        return (
+          <span>
+            <a onClick={e => toggleEditable(e, record.key)}>编辑</a>
+            <Divider type="vertical" />
+            <Popconfirm title="是否要删除此行？" onConfirm={() => remove(record.key)}>
+              <a>删除</a>
+            </Popconfirm>
+          </span>
+        );
+      },
+    },
+  ];
   return (
-    <HeaderDropdown overlay={langMenu} placement="bottomRight">
-      <span className={classNames(styles.dropDown, className)}>
-        <GlobalOutlined title="语言" />
-      </span>
-    </HeaderDropdown>
+    <>
+      <Table
+        loading={loading}
+        columns={columns}
+        dataSource={data}
+        pagination={false}
+        rowClassName={record => (record.editable ? styles.editable : '')}
+      />
+      <Button
+        style={{
+          width: '100%',
+          marginTop: 16,
+          marginBottom: 8,
+        }}
+        type="dashed"
+        onClick={newMember}
+      >
+        <PlusOutlined />
+        新增成员
+      </Button>
+    </>
   );
 };
 
-export default SelectLang;
+export default TableForm;
